858a641bae9e80960a5b881b83230b94443e66a0
etoraman <emretoraman@yandex.com>
2015-10-18 17:33:59 +0300

    sql

---
+++ nobet_yaz.php
@@ -0,0 +1,32 @@
+<?php
+include 'mysqlcon.php';
+include 'tarih_fonksiyonu.php';
+
+$tarih1 = $_POST['tarih1'];
+echo date('w',strtotime($tarih1));
+$tarih2 = $_POST['tarih2'];
+$nobet = $_POST['nobet'];
+$tarihler = tarih_fark_bul($tarih1,$tarih2,"-");
+
+	if($nobet == 'kat'){
+		$sorgu = mysql_query("SELECT kat.id,kat.numara,kat.tuttu,kat.son_tarih,ogr_listesi.evci_bekar FROM kat,ogr_listesi WHERE kat.numara = ogr_listesi.numara ORDER BY kat.tuttu ASC");
+		while($liste = mysql_fetch_assoc($sorgu)){
+			
+			$dizi_numara[] = $liste['numara'];  // Burada diziye atadık.
+			echo $liste['numara']."\t";
+			$dizi_tuttu[] = $liste['tuttu'];
+			echo $liste['tuttu']."\t";
+			$dizi_tarih [] = $liste['son_tarih'];
+			echo $liste['son_tarih']."\t";
+			$dizi_evci [] = $liste['evci_bekar'];
+			echo $liste['evci_bekar']."<br>";
+		} 
+		echo count($dizi_numara)."<br>".count($dizi_tuttu)."<br>".count($dizi_tarih)."<br>".count($dizi_evci);
+	}
+
+
+
+
+
+
+?>
--- tarih_fonksiyonu.php
+++ tarih_fonksiyonu.php
@@ -27,4 +27,20 @@ else if ($t2_timestamp > $t1_timestamp)
 }
 return $result;
 }
+function tarih_kontrol($tarih1,$tarih2,$ayrac){
+	list($y1,$a1,$g1) = explode($ayrac,$tarih1);
+	list($y2,$a2,$g2) = explode($ayrac,$tarih2);
+	$t1_timestamp = mktime('0','0','0',$a1,$g1,$y1);
+	$t2_timestamp = mktime('0','0','0',$a2,$g2,$y2);
+	if ($t1_timestamp > $t2_timestamp)
+	{
+		echo "buyuk";
+		return true;
+	}
+	if ($t2_timestamp > $t1_timestamp) 
+	{
+		echo "kucuk";
+		return false;
+	}
+}
 ?>
